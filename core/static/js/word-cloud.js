8714f5e613a44b37a5cd12fc404d6859bb678455
Bogdan Grigorovich <bohdan.hryhorovych@gmail.com>
2016-04-29 21:49:59 +0300

    Fix bootstrap .row width bug. Translate datepicker

--- core/static/js/word-cloud.js
+++ core/static/js/word-cloud.js
@@ -183,3 +183,13 @@ $(document).ready(function () {
     loadSitesSelect();
     loadWordCloud();
 });
+
+$.datepicker.regional['ua'] = {
+    monthNames: ['Січня','Лютого','Березня','Квітня','Травня','Червня', 'Липня','Серпня','Вересня','Жовтня','Листопада','Грудня'], // set month names
+    monthNamesShort: ['Січ','Лют','Бер','Кві','Тра','Чер','Лип','Сер','Вер','Жовт','Лист','Гру'],
+    dayNames: ['Неділя','Понеділок','Вівторок','Середа','Четвер',"П'ятниця",'Субота'],
+    dayNamesShort: ['Нд','Пн','Вт','Ср','Чт','Пт','Сб'],
+    dayNamesMin: ['Нд','Пн','Вт','Ср','Чт','Пт','Сб'],
+    dateFormat: 'dd/mm/yy'
+};
+$.datepicker.setDefaults($.datepicker.regional['ua']);
